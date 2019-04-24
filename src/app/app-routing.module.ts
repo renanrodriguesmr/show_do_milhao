ef0eed8e5bc73e6fb6ba1d014639a447f31ace79
renanrodriguesmr <renan.rodriguesmr@gmail.com>
2019-04-24 16:20:46 -0300

    initial view and bootstrap add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InitialViewComponent } from './initial-view/initial-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/start', pathMatch: 'full' },
+  { path: 'start', component: InitialViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
